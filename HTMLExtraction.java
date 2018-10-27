4d54e2f0637244ed0340ab8f04f6b2eac90a676c
slysleedor <44149818+slysleedor@users.noreply.github.com>
2018-10-26 19:43:36 -0500

    updated HTMLExtraction

--- HTMLExtraction.java
+++ HTMLExtraction.java
@@ -7,35 +7,82 @@ import org.jsoup.nodes.Document;
 
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+
 public class HTMLExtraction {
 	
+	private String url;
+	private String title;
+	private Document htmlDoc;
+	private Element body;
+
 	
 		public HTMLExtraction(){
 		
 
+					//URL to test
+		      String url = "https://en.wikipedia.org/wiki/Web_crawler";   
+		      setUrl(url);
+		      
+		      		//Document with HTML
+		      Document htmlDoc = establishConnection(url);
+
+		      		//web page title
+		      title =  htmlDoc.title();
+		      setTitle(title);
+	
+		      		//get html body and cleaned body
+		      Element body = htmlDoc.body();
+		      Elements paragraphs = setBody(body);
+		      
+		      		//print cleaned body text
+		      for (Element paragraph : paragraphs) {
+		         System.out.println(paragraph.text());
+		      }
+		}
 		
-		      String url = "https://en.wikipedia.org/wiki/Web_crawler";
-		      Document document = null;
+				//Connect to web page and extract HTML
+		public Document establishConnection(String url){
+			
 			try {
-				document = Jsoup.connect(url).get();
+				htmlDoc = Jsoup.connect(url).get();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		   //   System.out.println(document.body());
-		      
-		      
-	
-		      Element body = document.body();
-		      Elements paragraphs = body.getElementsByTag("p");
-		      
-		      
-		      for (Element paragraph : paragraphs) {
-		         System.out.println(paragraph.text());
-		      }
-		}  
+			
+			return htmlDoc;
+		}
 		
-
+		
+		public String getTitle(){
+			
+			return title;
+		}
+		
+		public void setTitle(String title){
+			
+			this.title = title;
+		}
+		
+		public Elements setBody(Element body){
+			
+			Elements paragraphs = body.getElementsByTag("p");
+			
+			return paragraphs;
+		}
+		
+		public String getUrl(){
+			
+			return url;
+		}
+		
+		public void setUrl(String url){
+			
+			this.url = url;
+		}
+		
+		
 	}
 	
 
