1ee2b498bda6517ebeba8eac58fc64b1308168c5
NicholasLookabaugh <32078028+NicholasLookabaugh@users.noreply.github.com>
2018-11-16 21:28:40 -0600

    Updated the UI for more functionality
    Added a search length field to allow the user to choose the amount of sites for the crawler to visit. Also added error handling boxes if the search term field or the search amount field are left empty.

--- src/WindowsApp/CrawlerApp.java
+++ src/WindowsApp/CrawlerApp.java
@@ -7,10 +7,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 
 import TextCorpusAquisition.HTMLExtraction;
 import GoogleSearch.GoogleCustomSearch;
@@ -52,27 +54,71 @@ public class CrawlerApp {
 	 */
 	private void initialize() {
 		frame = new JFrame("Text Corpus Acquisition");
-		frame.setSize(500, 200);
+		frame.setSize(750, 225);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(null);
 		
 		JButton crawlButton = new JButton("Crawl");
-		crawlButton.setBounds(25, 50, 100, 50);
+		crawlButton.setBounds(25, 75, 100, 50);
 		
-		JTextField inputText = new JTextField();
-		inputText.setBounds(150, 50, 300, 50);
+		JTextField searchTerm = new JTextField();
+		searchTerm.setBounds(150, 75, 300, 50);
+		
+		MySearchAmountField searchAmount = new MySearchAmountField();
+		searchAmount.setBounds(500, 75, 150, 50);
+		
+		JTextField searchText = new JTextField("Enter your search term");
+		searchText.setBounds(225, 25, 150, 50);
+		searchText.setEditable(false);
+		searchText.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+		
+		JTextField amountText = new JTextField("Enter the search length");
+		amountText.setBounds(512, 25, 150, 50);
+		amountText.setEditable(false);
+		amountText.setBorder(javax.swing.BorderFactory.createEmptyBorder());
 
+		frame.add(searchText);
+		frame.add(amountText);
 		frame.add(crawlButton);
-		frame.add(inputText);
+		frame.add(searchTerm);
+		frame.add(searchAmount);
 		
 		crawlButton.addActionListener(new ActionListener()
 		{
 		  public void actionPerformed(ActionEvent e)
 		  {
-			  GoogleCustomSearch crawl = new GoogleCustomSearch();
-			  crawl.search(inputText.getText());
+			  if(!searchTerm.getText().isEmpty())
+			  {
+				  try
+				  {
+					  Integer.parseInt(searchAmount.getText());
+					  //GoogleCustomSearch crawl = new GoogleCustomSearch();
+					  //crawl.search(searchTerm.getText());
+					  System.out.println(searchAmount.getIntValue());
+				  }
+				  catch (NumberFormatException f)
+				  {
+					  JOptionPane.showMessageDialog(frame, "A number must be inputted in the search length field.", "Error", JOptionPane.ERROR_MESSAGE);
+				  }
+			  }
+			  else
+			  {
+				  JOptionPane.showMessageDialog(frame, "The search term field cannot be left empty.", "Error", JOptionPane.ERROR_MESSAGE);
+			  }
 		  }
 		});
 	}
+}
 
+class MySearchAmountField extends JTextField
+{
+	private static final long serialVersionUID = 1L;
+	public void setIntValue(int x)
+    {
+        setText(""+x);
+    }
+    public int getIntValue()
+    {
+        return Integer.parseInt(getText());
+    }
 }
